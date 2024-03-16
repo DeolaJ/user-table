5d695916d07ab95cd85a366f41caac99196455d4
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2024-03-16 10:35:45 +0000

    feat: add queryclient and chakra provider to main app
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { theme } from "./styles/theme";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 function App() {
     return (
-        <>
-            <div>Hello world</div>
-        </>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <div>Hello World</div>
+            </ChakraProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
