c6f99d326237c7e9d16d32e86132541991b0f56a
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2024-03-16 15:20:04 +0000

    feat: add User page component to main app
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import UsersPage from "./components/UserPage";
+import ErrorBoundary from "./components/shared/ErrorBoundary";
+
 import { theme } from "./styles/theme";
 
 const queryClient = new QueryClient({
@@ -16,7 +19,9 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
-                <div>Hello World</div>
+                <ErrorBoundary>
+                    <UsersPage />
+                </ErrorBoundary>
             </ChakraProvider>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
